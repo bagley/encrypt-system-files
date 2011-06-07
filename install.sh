44177e7df16c6f40d3a5e86f4aed05187f6a944d
Matt Smith <shadowfax@gmx.com>
2011-06-07 10:13:41 -0600

    Changed from lib to share. Uninstall leaves config.

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ PREFIX=/usr/local
 method="install"
 
 name=encrypt-system-files
+if [ -z "$name" ] ; then
+ echo "name was blank"
+ exit
+fi
 
 if [ $# -gt 0 ] ; then
  case $@ in
@@ -40,6 +44,7 @@ gpg=$exe
 check_exec install
 install=$exe
 
+
 if [ "$method" == "uninstall" ] ; then
   echo "Are you sure you want to uninstall $name? (n/y)"
   read RESP
@@ -50,28 +55,54 @@ if [ "$method" == "uninstall" ] ; then
   # uninstall
   set -x
   rm -f "$PREFIX/bin/$name"
-  rm -f "$PREFIX/lib/$name/home"*
-  rmdir "$PREFIX/lib/$name/home"
+  rm -f "$PREFIX/share/$name/home/"*
+  rmdir "$PREFIX/share/$name/home" 2>/dev/null
 # leave the config...
-#  rm -f "$PREFIX/lib/$name/"*
-#  rmdir "$PREFIX/lib/$name"
+#  rm -f "$PREFIX/share/$name/"*
+#  rmdir "$PREFIX/share/$name"
+  # for old method of install
+  if [ -d "$PREFIX/lib/$name" ] ; then
+    rm -f "$PREFIX/lib/$name/home/"*
+    rmdir "$PREFIX/lib/$name/home" 2>/dev/null
+    [ -f "$PREFIX/lib/$name/config" ] && oldconfig=1
+    # and move config to share, unless it already exists
+    #if ! [ -f "$PREFIX/share/$name/config" ] && [ -f "$PREFIX/lib/$name/config" ] ; then
+    #  mkdir -p -m 0700 "$PREFIX/share/$name"
+    #  mv "$PREFIX/lib/$name/config" "$PREFIX/share/$name/"
+    #  rmdir --ignore-fail-on-non-empty "$PREFIX/share/$name"
+    #fi
+  fi
   set +x
+  echo
+  if [ -n "$oldconfig" ] ; then
+    echo "Old config was left at $PREFIX/lib/$name/config"
+  fi
+  echo "Config was left at $PREFIX/share/$name/config"
   echo "Done"
 
 else
   # install
-  echo "This will install $name in $PREFIX[/bin|/lib]. Continue? (y/n)"
+  echo "This will install $name in $PREFIX[/bin|/share]. Continue? (y/n)"
   read RESP
   if [ "$RESP" != "y" ] ; then
     echo "Nothing done"
     exit
   fi
+  if [ -f  "$PREFIX/share/$name/config" ] ; then
+    echo
+    echo "Config file already exists. If you want to overwrite it, please run the following command:"
+    echo "cp ./config \"$PREFIX/share/$name/config\""
+    echo
+  else
+    CPCONFIG="install config"
+  fi
   set -x
   $install -o root -g root -m 0700 "./$name" "$PREFIX/bin/$name"
-  mkdir -p -m 0700 "$PREFIX/lib/$name"
-  mkdir -p -m 0700 "$PREFIX/lib/$name/home"
-  $install -o root -g root -m 0600 "./config" "$PREFIX/lib/$name/config"
-  $gpg --homedir "$PREFIX/lib/$name/home" --import ./PublicKey_to_Encrypt
+  mkdir -p -m 0700 "$PREFIX/share/$name"
+  mkdir -p -m 0700 "$PREFIX/share/$name/home"
+  # do not install config file if it is already there
+  [ -n "$CPCONFIG" ] && $install -o root -g root -m 0600 "./config" "$PREFIX/share/$name/config"
+  $gpg --homedir "$PREFIX/share/$name/home" --import ./PublicKey_to_Encrypt
   set +x
   echo "Done. Be sure to check config and edit accordingly."
 fi
